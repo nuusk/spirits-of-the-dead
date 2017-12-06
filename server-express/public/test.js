586b34d2a5546d6dddc0ad68e68cf4b52bdafaca
Piotr Ptak <robakzdobywca@gmail.com>
2017-12-06 02:21:33 +0100

    chrome sockets experimental api

--- server-express/public/test.js
+++ server-express/public/test.js
@@ -1,13 +1,30 @@
-var socket = new WebSocket("ws://localhost:1252");
+// const address = "192.168.8.106";
+const address = "localhost";
+const port = "1252";
+const button = document.getElementById('send');
+
+let socket = new WebSocket("ws://"+address+":"+port);
 
 socket.onmessage = function(event){
-    console.log("ONMESSAGE: " + event.data);
+  console.log("ONMESSAGE: " + event.data);
 }
 
 socket.onopen = function(event){
-    console.log("Socket: Connected!")
+  console.log("Socket: Connected!")
 }
 
 socket.onclose = function(event){
-    console.log("Socket: Connection closed!");
+  console.log("Socket: Connection closed!");
 }
+
+button.onclick = () => {
+  socket.send("dzień dobry");
+};
+
+
+// chrome.sockets.tcp.create({}, function(createInfo) {
+//   chrome.sockets.tcp.connect(createInfo.socketId,
+//     "192.168.8.106", 1252, function(socketInfo) {
+//
+//     });
+// });
