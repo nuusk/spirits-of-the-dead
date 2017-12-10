660bae90e0c091c86f653191819033c6ede9d4e5
Piotr Ptak <robakzdobywca@gmail.com>
2017-12-10 01:35:05 +0100

    all possible answers

--- bsd-server/jsonReader.cpp
+++ bsd-server/jsonReader.cpp
@@ -11,10 +11,9 @@ public:
   std::string npc;
   std::string text;
   std::vector<std::string> answers;
-
 };
 
-int findFor(std::string word, std::string line) {
+int searchFor(std::string word, std::string line) {
   std::size_t wordPosition = line.find(word);
   if (wordPosition!=std::string::npos) {
     return wordPosition;
@@ -27,51 +26,62 @@ int main() {
   std::string line;
   std::ifstream input("../lore/story.txt");
   int entries = 0;
+
+  //this object contains the whole 'story' written in text.
+  //type (where it can happen)
+  //id (used to determine where the encounter can happen)
+  //npc (who will say the encounter text)
+  //text (what will be actually said)
+  //answers (list of possible answers to this question)
   std::vector<Entry> story;
+
   bool readingAnswers = false;
 
   while (std::getline(input, line)) {
     if (readingAnswers) {
-      std::cout<<line<<std::endl;
-      readingAnswers = false;
+      if (searchFor("]", line) != -1) {
+        readingAnswers = false;
+        continue;
+      }
+      // std::cout<<line<<std::endl;
+      std::string currentAnswer = line.substr(searchFor(":", line)+3);
+      currentAnswer.erase(currentAnswer.end()-2, currentAnswer.end());
+      currentAnswer.erase(currentAnswer.begin(), currentAnswer.begin()+3);
+      story[entries].answers.push_back(currentAnswer);
       continue;
     }
-    if (findFor("{", line) != -1) {
+    if (searchFor("{", line) != -1) {
       story.push_back(*(new Entry()));
-      // std::cout << findFor("{", line) << std::endl;
+      // std::cout << searchFor("{", line) << std::endl;
     }
-    if (findFor("type", line) != -1) {
-      story[entries].type = line.substr(findFor(":", line)+3);
+    if (searchFor("}", line) != -1) {
+      entries++;
+      // std::cout << searchFor("{", line) << std::endl;
+    }
+    if (searchFor("type", line) != -1) {
+      story[entries].type = line.substr(searchFor(":", line)+3);
       story[entries].type.erase(story[entries].type.end()-2, story[entries].type.end());
-      //std::cout << findFor("text", line) << std::endl;
+      //std::cout << searchFor("text", line) << std::endl;
     }
-    if (findFor("story-id", line) != -1) {
-      story[entries].storyID = line.substr(findFor(":", line)+3);
+    if (searchFor("story-id", line) != -1) {
+      story[entries].storyID = line.substr(searchFor(":", line)+3);
       story[entries].storyID.erase(story[entries].storyID.end()-2, story[entries].type.end());
-      //std::cout << findFor("text", line) << std::endl;
+      //std::cout << searchFor("text", line) << std::endl;
     }
-    if (findFor("npc", line) != -1) {
-      story[entries].npc = line.substr(findFor(":", line)+3);
+    if (searchFor("npc", line) != -1) {
+      story[entries].npc = line.substr(searchFor(":", line)+3);
       story[entries].npc.erase(story[entries].npc.end()-2, story[entries].npc.end());
-      //std::cout << findFor("text", line) << std::endl;
+      //std::cout << searchFor("text", line) << std::endl;
     }
-    if (findFor("text", line) != -1) {
-      story[entries].text = line.substr(findFor(":", line)+3);
+    if (searchFor("text", line) != -1) {
+      story[entries].text = line.substr(searchFor(":", line)+3);
       story[entries].text.erase(story[entries].text.end()-2, story[entries].text.end());
-      //std::cout << findFor("text", line) << std::endl;
+      //std::cout << searchFor("text", line) << std::endl;
     }
-    if (findFor("answers", line) != -1) {
+    if (searchFor("answers", line) != -1) {
       readingAnswers = true;
     }
   }
 
-  // std::cout<<story[entries].storyID<<std::endl;
+  std::cout<<story[entries].answers[1]<<std::endl;
 }
-// "type": "story",
-// "storyID": "1",
-// "npc": "gm",
-// "text": "You are at the tavern with your best friends. What do you intend to do?",
-// "answers": [
-//   "a": "Go for an adventure.",
-//   "b": "Let's stay here and relax."
-// ]
