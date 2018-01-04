1e8d50f76294c0bcf4ebe698d60808604083ccf0
Piotr Ptak <robakzdobywca@gmail.com>
2018-01-04 13:21:27 +0100

    background music + brown paper texture bg

--- app/js/characterSelect.js
+++ app/js/characterSelect.js
@@ -13,32 +13,15 @@ function removeOthers(index) {
   for (let i=0; i<numberOfCharacters-index-1; i++) {
     menu.removeChild(characters[1]);
   }
-  // moveCenter(characters);
-}
-
-function moveCenter(chosenCharacter) {
-  // chosenCharacter.parentNode.style.display = 
-  var pos = 0;
-  console.log(chosenCharacter);
-  var id = setInterval(()=>{
-    // console.log(pos);
-    if (pos == 350) {
-      clearInterval(id);
-    } else {
-      pos++;
-      chosenCharacter.style.top = pos + 'px';
-      chosenCharacter.style.left = pos + 'px';
-    }
-  }, 10);
 }
 
 for (let i=0; i<characters.length; i++) {
   characters[i].addEventListener('click', ()=>{
     const characterName = characters[i].id;
+    // menu.className += ' selected';
     // removeOthers(i);
-    moveCenter(this);
     setTimeout(()=>{
       ipcRenderer.send('character:select', characterName);
-    }, 40500);
+    }, 800);
   });
 }
