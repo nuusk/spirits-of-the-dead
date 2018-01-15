7460b2b9380150361404402d5591450cece16fef
Piotr Ptak <robakzdobywca@gmail.com>
2018-01-15 21:08:40 +0100

    game view composition change

--- app/js/game.js
+++ app/js/game.js
@@ -3,13 +3,14 @@ const {ipcRenderer} = electron;
 
 const wall = document.getElementById('wall');
 const commandLine = document.getElementById('command-line');
-const readyButton = document.getElementById('readyButton');
+const readyButton = document.getElementById('set-ready');
 const name = document.getElementById('name');
-const nameButton = document.getElementById('nameButton');
-const onlineLabel = document.getElementById('playersOnline');
-const readyLabel = document.getElementById('playersReady');
-const answerNumber = document.getElementById('answerNumber');
-const mostPopularAnswer = document.getElementById('mostPopularAnswer');
+const nameButton = document.getElementById('set-name');
+const username = document.getElementById('username');
+const onlineLabel = document.getElementById('players-online');
+const readyLabel = document.getElementById('players-ready');
+const answerNumber = document.getElementById('answer-number');
+const mostPopularAnswer = document.getElementById('most-popular-answer');
 
 const lobbyDiv = document.getElementById('lobbyDiv');
 const answersDiv = document.getElementById('answersDiv');
@@ -27,13 +28,12 @@ window.onload = () => {
 
 //~~~~~~~~~~~~~~~~~~~~~ Events ~~~~~~~~~~~~~~~~~~~~~~~
 commandLine.addEventListener('keydown', (e)=>{
-  if (e.keyCode == 13) 
-  {
+  if (e.keyCode == 13) {
     if (chatting)
       ipcRenderer.send('chat', commandLine.value);
     else
       ipcRenderer.send('terminal:command', commandLine.value);
-  
+
     commandLine.value = '';
   }
 });
@@ -41,7 +41,7 @@ commandLine.addEventListener('keydown', (e)=>{
 readyButton.addEventListener('click', () => {
   if (!ready)
     readyButton.value = 'Not ready';
-  else 
+  else
     readyButton.value = 'Ready';
 
   ready = !ready;
@@ -49,15 +49,18 @@ readyButton.addEventListener('click', () => {
 });
 
 nameButton.addEventListener('click', () => {
-  if (name.value.trim() != '')
-  {
-    ipcRenderer.send('setName', name.value);
-    name.value = "";
-    name.disabled = true;
+  if (username.value.trim() != '') {
+    ipcRenderer.send('setName', username.value);
+    // username.value = "";
+    username.disabled = true;
     nameButton.disabled = true;
   }
 });
 
+username.addEventListener('focus', () => {
+  console.log('yo');
+  this.value = '';
+});
 
 //~~~~~~~~~~~~~~~~~~~~~ ipcRenderer ~~~~~~~~~~~~~~~~~~~~~~~
 ipcRenderer.on('gameStartedInfo', (e, data) => {
@@ -75,7 +78,7 @@ ipcRenderer.on('playersLobbyInfo', (e, data) => {
     onlineLabel.innerHTML = `In game: ${data.inGame}`;
     readyLabel.innerHTML = `Waiting: ${data.waiting}`;
     readyButton.style.display = 'none';
-  } 
+  }
 });
 
 ipcRenderer.on('answersInfo', (e, data) => {
@@ -99,10 +102,10 @@ ipcRenderer.on('gameStart', () => {
 ipcRenderer.on('gameEnd', () => {
   gameStarted = false;
   chatting = true;
-  answersDiv.style.display = 'none';  
+  answersDiv.style.display = 'none';
   lobbyDiv.style.display = 'block';
-  readyButton.style.display = 'block';  
-  
+  readyButton.style.display = 'block';
+
   ready = false;
   readyButton.value = 'Ready';
 
@@ -110,25 +113,25 @@ ipcRenderer.on('gameEnd', () => {
 
   wall.value += '--------- THE END ---------';
   wall.value += '\n--------- THE END ---------';
-  wall.value += '\n--------- THE END ---------\n\n\n\n\n'; 
+  wall.value += '\n--------- THE END ---------\n\n\n\n\n';
   wall.scrollTop = wall.scrollHeight;
-  wall.focus(); 
+  wall.focus();
 });
 
 ipcRenderer.on('stage', (e, data) => {
   commandLine.value = "";
   commandLine.disabled = true;
-  
+
   setTimeout(() => {
     commandLine.disabled = false;
     commandLine.focus();
   }, 500);
 
   wall.value += '\n' + data.text + '\n';
-  
+
   for (let i = 0; i < data.answers.length; i++)
     wall.value += `${i+1}. ${data.answers[i]}\n`;
-  
+
   wall.value += '\n';
   wall.scrollTop = wall.scrollHeight;
 });
